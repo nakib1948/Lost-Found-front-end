cae8b303ce72a4af38741f940e310ea8f8576b55
nakib1948 <hayathossain.jtp@gmail.com>
2024-05-22 06:48:29 +0600

    project initialized

---
+++ src/Components/shared/Navbar/Navbar.tsx
@@ -0,0 +1,121 @@
+"use client"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Stack } from '@mui/material';
+
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+}
+
+const drawerWidth = 240;
+const navItems = ['Home', 'About'];
+
+export default function Navbar(props: Props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }} mb={10}>
+      <CssBaseline />
+      <AppBar component="nav" sx={{background:"none"}}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' },mx:"auto" }}>
+            <Stack gap={4} direction={"row"} >
+            {navItems.map((item) => (
+              <Typography key={item} sx={{ color: '#fff' }} >
+                {item}
+              </Typography>
+            ))}
+            </Stack>
+            
+          </Box>
+          
+            <Stack gap={2} direction={"row"} >
+              <Button color="success">Login</Button>
+            </Stack>
+            
+          
+        </Toolbar>
+        
+      </AppBar>
+      
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+         
+        </Drawer>
+      
+      </nav>
+     
+    </Box>
+  );
+}
---
+++ src/lib/theme/theme.ts
@@ -0,0 +1,35 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  // palette: {
+  //   primary: {
+  //     main: "#4a536b",
+  //   },
+  //   secondary: {
+  //     main: "#aed6dc",
+  //   },
+  // },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        variant: "contained",
+      },
+      styleOverrides:{
+        root:{
+            padding:'4px 24px'
+        }
+      }
+    },
+    MuiContainer:{
+        defaultProps:{
+            maxWidth:'lg'
+        }
+    }
+  },
+  typography:{
+    body1:{
+        color:"#333333"
+    }
+  }
+});
+theme.shadows[1] = "0px 6px 20px lightgray"
